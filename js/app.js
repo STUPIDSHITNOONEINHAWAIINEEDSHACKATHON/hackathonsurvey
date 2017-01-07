dcd149faee644954e26f9d67dc33b02334240c39
Estefania Morton <estefania.morton@gmail.com>
2017-01-07 13:28:32 -1000

    multiple questions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*jshint eversion:6*/
+let form = document.getElementById('form');
 
 let module = function() {
 
@@ -36,7 +37,35 @@ let module = function() {
     }
   }
 
+  function randomizeCorrectAns(array, correctAns){
+     let index = Math.floor(Math.random() * (array.length + 1))
+     array.splice(index, 0, correctAns);
+     return array;
+  }
+
   function addMult(data) {
+    let multDiv = document.createElement('div');
+    multDiv.classList.add('div-mult');
+    let label = document.createElement('label');
+    label.classList.add('label-mult');
+    label.innerHTML = data.question;
+    let optionsDiv = document.createElement('div');
+    let answers = randomizeCorrectAns(data.incorrect_answers, data.correct_answer);
+
+    for(let i = 0; i < answers.length; i++) {
+      let inputLabel = document.createElement('label');
+      let input = document.createElement('input');
+      let span = document.createElement('span');
+      span.innerHTML = data.incorrect_answers[i];
+      input.setAttribute('type', 'radio');
+      input.setAttribute('name', 'answer');
+      inputLabel.appendChild(input);
+      inputLabel.appendChild(span);
+      optionsDiv.appendChild(inputLabel);
+    }
+    multDiv.appendChild(label);
+    multDiv.appendChild(optionsDiv);
+    form.appendChild(multDiv);
 
   }
 
