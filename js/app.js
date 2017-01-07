80af8efea09b8c12bc6c93b5a4520aef3f378bef
raaarwwwwandy <hirandysan@gmail.com>
2017-01-07 12:23:36 -1000

    some index work

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 /*jshint eversion:6*/
-let questionReq = new XMLHttpRequest();
-questionReq.addEventListener('load', questionReqListener);
-questionReq.open('GET', './data/tmp.json');
-questionReq.send();
 
 let module = function() {
 
-  function addMult() {
+  function requestHandler(){
+    var data = JSON.parse(this.responseText);
+    console.log(data);
 
   }
 
-  function addTrueFalse() {
+  function requestData(){
+    let questionReq = new XMLHttpRequest();
+    questionReq.addEventListener('load', requestHandler);
+    questionReq.open('GET', './data/tmp.json');
+    questionReq.send();    
+  }
+
+  function addMult(data) {
+
+
+  }
+
+  function addTrueFalse(data) {
 
   }
 
@@ -19,10 +29,12 @@ let module = function() {
   }
 
   function addOpen() {
+    
 
   }
 
   return {
+    requestData,
     addMult,
     addTrueFalse,
     addCheckbox,
@@ -31,4 +43,8 @@ let module = function() {
 
 };
 
-module();
+
+
+const app = module();
+
+app.requestData();
