48fae61416a52fbea6c1cc336a33b278d8c27929
Estefania Morton <estefania.morton@gmail.com>
2017-01-07 15:20:13 -1000

    background color

--- js/app.js
+++ js/app.js
@@ -1,36 +1,39 @@
 /*jshint eversion:6*/
-
 let form = document.getElementById('content');
 let btn = document.getElementById('nextBtn');
 
 let module = function() {
 
+  let colorIndex = 0;
   let startQ = 0;
   let endQ = 5;
   let totalQ = 0;
   let questions;
 
+
   btn.addEventListener('click', addBtnListener);
 
+  let colors = ['#9EFF36', '#303030', '#FA5BA5', '#42B983', '#E6834A', '#9373C1', '#00B8A9', '#FF0000']
 
   function addBtnListener(event) {
     event.preventDefault();
     startQ = endQ;
-    endQ += 5;
+    endQ += Math.floor(Math.random() * 10);
     if(endQ > totalQ) {
       endQ = totalQ;
     }
+    colorIndex++;
     processData(questions)
+    if(colorIndex > 9) {
+      colorIndex = 0;
+    }
   }
-
   function processData(data){
     for(let i = startQ; i < endQ; i++){
       let index = i;
       checkType(data[i], index);
     }
   }
-
-
   function hackReqHandler(){
     let data = JSON.parse(this.responseText);
     data = data.results;
@@ -38,50 +41,40 @@ let module = function() {
     totalQ = data.length;
     console.log(endQ, totalQ);
     processData(data);
-
   }
-
   function requestData(endpoint) {
     let questionReq = new XMLHttpRequest();
     questionReq.addEventListener('load', hackReqHandler);
     questionReq.open('GET', endpoint);
     questionReq.send();
   }
-
-
   function checkType(data, index) {
     switch(data.type) {
       case 'multiple':
         addMult(data, index);
         break;
-
       case 'boolean':
         addTrueFalse(data, index);
         break;
-
       case 'open':
       default:
         addOpen(data, index);
     }
   }
-
   function randomizeCorrectAns(array, correctAns){
      let index = Math.floor(Math.random() * (array.length + 1))
      array.splice(index, 0, correctAns);
      return array;
   }
-
-
   function addMult(data, index) {
     let multDiv = document.createElement('div');
     multDiv.classList.add('div-mult');
+    multDiv.style.backgroundColor = colors[colorIndex];
     let label = document.createElement('label');
     label.classList.add('label-mult');
     label.innerHTML = data.question;
     let optionsDiv = document.createElement('div');
-
     let answers = randomizeCorrectAns(data.incorrect_answers, data.correct_answer);
-
     for(let i = 0; i < answers.length; i++) {
       let inputLabel = document.createElement('label');
       let input = document.createElement('input');
@@ -96,22 +89,17 @@ let module = function() {
     multDiv.appendChild(label);
     multDiv.appendChild(optionsDiv);
     form.appendChild(multDiv);
-
   }
-
-
   function addTrueFalse(data, index) {
     let array = data.incorrect_answers;
     array.splice(1, 0, data.correct_answer);
-
     let boolDiv = document.createElement('div');
     boolDiv.classList.add('div-bool');
+    boolDiv.style.backgroundColor = colors[colorIndex];
     let label = document.createElement('label');
     label.classList.add('label-bool');
     label.innerHTML = data.question;
     let optionsDiv = document.createElement('div');
-
-
     for(let i = 0; i < data.incorrect_answers.length; i++) {
       let inputLabel = document.createElement('label');
       let input = document.createElement('input');
@@ -126,37 +114,27 @@ let module = function() {
     boolDiv.appendChild(label);
     boolDiv.appendChild(optionsDiv);
     form.appendChild(boolDiv);
-
   }
-
-
   function addOpen(data) {
     let openDiv = document.createElement('div');
     openDiv.classList.add('div-open');
+    openDiv.style.backgroundColor = colors[colorIndex];
     let label = document.createElement('label');
-    label.classList.add('labe-open');
+    label.classList.add('label-open');
     label.innerHTML = data.question;
     let input = document.createElement('input');
-    let inputLabel = document.createElement('label');
     input.setAttribute('type', 'text');
-    inputLabel.appendChild(input);
-    openDiv.appendChild(inputLabel);
+    openDiv.appendChild(label);
+    openDiv.appendChild(input);
     form.appendChild(openDiv);
-
   }
-
   return {
     requestData,
     addMult,
     addTrueFalse,
     addOpen,
   }
-
 };
-
-
-
 const app = module();
-
 // app.requestData( './data/tmp.json');
 app.requestData( './data/hackathon.json');
